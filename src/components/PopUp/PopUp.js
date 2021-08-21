f89e12e62bae4faf41be92a7d9b90ebf745edc59
Hanna Mdanat <yuhanna.almadanat@optimumpartners.co>
2021-08-21 12:53:30 +0300

    potato

--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -3,17 +3,21 @@ import './PopUp.css'
 import axios from 'axios'
 
 export default function PopUp({ pokemon }) {
-    const [pokemonsData, setPokemonsData] = useState({});
+    const [pokemons, setPoki] = useState({});
         useEffect(() => {
         axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.id}`)
         .then(response => {
             const data = response.data;
-            setPokemonsData(data);
+            setPoki(data);
         }).catch();
     }, []);
 
+    function popUpClose() {
+        document.getElementById('poki').classList.add('close');
+    }
+
     return (
-        <div className='pop-up-wrapper '>
+        <div onClick={() => popUpClose()} id='poki' className='pop-up-wrapper'>
             <div className='content'>
                 <div className='modal-header'>
                     <h1 className='modal-title'>{pokemon.name}</h1>
@@ -22,8 +26,8 @@ export default function PopUp({ pokemon }) {
                     <img className='img' src={pokemon.sprites['front_default']} />
                     <div className='vertical'>
                         <h2>{`HP : ${100}`}</h2>
-                        <h2>{`Weight : ${pokemonsData.weight}`}</h2>
-                        <h2>{`Height : ${pokemonsData.height}`}</h2>
+                        <h2>{`Weight : ${pokemons.weight}`}</h2>
+                        <h2>{`Height : ${pokemons.height}`}</h2>
                         <h2>{`Attack : ${25}`}</h2>
                     </div>
                 </div>
